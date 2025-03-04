d4250be17c2631b94e5df68329a8eacc17afa915
Aron Gimesi <gimesiaron@gmail.com>
2025-03-04 22:48:14 +0100

    docs: typehints

--- gimdog_toolbox/files.py
+++ gimdog_toolbox/files.py
@@ -1,6 +1,7 @@
 import os
+from typing import List, Optional
 
-def get_all_filepaths(directory, extensions=None):
+def get_all_filepaths(directory: str, extensions: Optional[List[str]] = None) -> List[str]:
     filepaths = []
     for root, dirs, files in os.walk(directory):
         filepaths.extend(
@@ -10,20 +11,18 @@ def get_all_filepaths(directory, extensions=None):
         )
     return filepaths
 
-def get_first_level_subdirectories(directory):
+def get_first_level_subdirectories(directory: str) -> List[str]:
     subdirectories = []
     subdirectories.extend(entry.path for entry in os.scandir(directory) if entry.is_dir())
     return subdirectories
 
-
-def get_all_subdirectories(directory):
+def get_all_subdirectories(directory: str) -> List[str]:
     subdirectories = []
     for root, dirs, files in os.walk(directory):
         subdirectories.extend(os.path.join(root, dir) for dir in dirs)
     return subdirectories
 
-
-def remove_string_from_filenames_in_directory(directory, removed_string):
+def remove_string_from_filenames_in_directory(directory: str, removed_string: str) -> None:
     for filename in os.listdir(directory):
         if removed_string in filename:
             new_filename = filename.replace(removed_string, "")
