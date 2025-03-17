b0ed8a190b312f1dfbecc933909ed63caa53b725
Aron Gimesi <gimesiaron@gmail.com>
2025-03-17 21:52:39 +0100

    docs: docstrings for files.py

--- gimdog_toolbox/files.py
+++ gimdog_toolbox/files.py
@@ -2,6 +2,16 @@ import os
 
 
 def get_all_filepaths(directory, extensions=None):
+    """
+    Get all file paths in a directory, optionally filtered by file extensions.
+
+    Args:
+        directory (str): The directory to search for files.
+        extensions (list of str, optional): List of file extensions to filter by. Defaults to None.
+
+    Returns:
+        list of str: List of file paths.
+    """
     filepaths = []
     for root, dirs, files in os.walk(directory):
         filepaths.extend(
@@ -13,6 +23,15 @@ def get_all_filepaths(directory, extensions=None):
 
 
 def get_first_level_subdirectories(directory):
+    """
+    Get the first-level subdirectories in a directory.
+
+    Args:
+        directory (str): The directory to search for subdirectories.
+
+    Returns:
+        list of str: List of first-level subdirectory paths.
+    """
     subdirectories = []
     subdirectories.extend(
         entry.path for entry in os.scandir(directory) if entry.is_dir()
@@ -21,6 +40,15 @@ def get_first_level_subdirectories(directory):
 
 
 def get_all_subdirectories(directory):
+    """
+    Get all subdirectories in a directory.
+
+    Args:
+        directory (str): The directory to search for subdirectories.
+
+    Returns:
+        list of str: List of all subdirectory paths.
+    """
     subdirectories = []
     for root, dirs, files in os.walk(directory):
         subdirectories.extend(os.path.join(root, dir) for dir in dirs)
@@ -28,6 +56,16 @@ def get_all_subdirectories(directory):
 
 
 def remove_string_from_filenames_in_directory(directory, removed_string):
+    """
+    Remove a specific string from filenames in a directory.
+
+    Args:
+        directory (str): The directory containing the files.
+        removed_string (str): The string to remove from filenames.
+
+    Returns:
+        None
+    """
     for filename in os.listdir(directory):
         if removed_string in filename:
             new_filename = filename.replace(removed_string, "")
